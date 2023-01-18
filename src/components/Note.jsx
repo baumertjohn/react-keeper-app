bec732fad349624efe5de5e0322b23594e8f7d53
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2023-01-17 21:41:06 -0700

    Add example notes data file and change note to dynamic display.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>Test Note Title</h1>
-      <p>Test note text.</p>
+      <h1>{props.noteTitle}</h1>
+      <p>{props.noteContent}</p>
     </div>
   );
 }
