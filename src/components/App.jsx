d700cec054b23fcb80df5830e652ba32afcee485
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2023-02-01 20:27:25 -0700

    Add note enter area. Add delete button to note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((note) => (
         <Note
           key={note.key}
