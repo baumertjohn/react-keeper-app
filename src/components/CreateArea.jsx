e59e50417cdeea2f3f61aef98af87dea940c3fcd
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2023-02-03 22:21:27 -0700

    Add note add function. Add note delete function. Update delete button style.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [noteInput, setNoteInput] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleInput(event) {
+    const { name, value } = event.target;
+    setNoteInput((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function addNote(event) {
+    props.onAdd(noteInput);
+    setNoteInput({ title: "", content: "" });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note" />
-        <button>Add</button>
+        <input
+          onChange={handleInput}
+          name="title"
+          value={noteInput.title}
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleInput}
+          name="content"
+          value={noteInput.content}
+          placeholder="Take a note..."
+        />
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
